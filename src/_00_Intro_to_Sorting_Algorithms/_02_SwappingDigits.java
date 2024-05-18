b66a0d5abf6c3dc26b66e92a9da062e87430fc0f
league <league@192.168.1.78>
2024-05-18 11:30:54 -0700

    started module. Finished up to swappingDigits, need to fix test3

--- src/_00_Intro_to_Sorting_Algorithms/_02_SwappingDigits.java
+++ src/_00_Intro_to_Sorting_Algorithms/_02_SwappingDigits.java
@@ -5,20 +5,39 @@ class _02_SwappingDigits {
     // 1. Complete the method so that the first two elements of the array are
     //    swapped.
     public static void swapArrayOfTwo(int[] arr) {
-
+    	int temp0 = arr[0];
+    	arr[0]=arr[1];
+    	arr[1]=temp0;
     }
 
     // 2. Complete the method so that it sorts the array using a bubble sort.
     //    Iterate through the array and when you find two elements that are out
     //    of order, swap them. Repeat this until the array is in order.
     public static void sortIntArray(int[] arr) {
-
+    	boolean allSorted=false;
+    	while(allSorted==false) {
+    		allSorted=true;
+    	for(int i = 0;i<arr.length-1;i++) {
+    		if(arr[i]>arr[i+1]) {
+    			int temp = arr[i];
+    	    	arr[i]=arr[i+1];
+    	    	arr[i+1]=temp;
+    	    	allSorted=false;
+    		}
+    	}
+    	}
     }
 
     // 3. Complete the method so that it finds the middle number in the array.
     //    *Hint* it helps to sort it first.
     //    *Double Hint* Use the method you already wrote in step 2 to sort it
     public static int findMiddle(int[] arr) {
-        return 0;
+    	int[] array = arr;
+    	sortIntArray(array);
+    	if(array.length%2==0) {
+    		return array[array.length/2];
+    	} else {
+    		return array[array.length+1/2];
+    	}
     }
 }
